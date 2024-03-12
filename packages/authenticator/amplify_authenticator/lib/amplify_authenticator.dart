a161bbec8394753c77d144dd27dac219ce33473c
Kamalpreet Mundi <kamalmundi31@gmail.com>
2024-03-12 15:44:20 -0600

    scaffold update

--- packages/authenticator/amplify_authenticator/lib/amplify_authenticator.dart
+++ packages/authenticator/amplify_authenticator/lib/amplify_authenticator.dart
@@ -827,6 +827,7 @@ class _AuthenticatorBody extends StatelessWidget {
               child: ScaffoldMessenger(
                 key: _AuthenticatorState.scaffoldMessengerKey,
                 child: Scaffold(
+                  resizeToAvoidBottomInset: false,
                   body: SizedBox.expand(
                     child: child,
                   ),
@@ -866,6 +867,7 @@ class AuthenticatedView extends StatelessWidget {
           return ScaffoldMessenger(
             key: _AuthenticatorState.scaffoldMessengerKey,
             child: Scaffold(
+              resizeToAvoidBottomInset: false,
               body: SizedBox.expand(
                 child: child is AuthenticatorScreen
                     ? SingleChildScrollView(child: child)
