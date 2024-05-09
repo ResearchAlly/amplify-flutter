552b1f9174a8ee9d23bb8342319d7f0f0d0afa78
Kamalpreet Mundi <kamalmundi31@gmail.com>
2024-05-08 20:49:36 -0600

    Update errorMaxLines

--- packages/authenticator/amplify_authenticator/lib/src/mixins/authenticator_text_field.dart
+++ packages/authenticator/amplify_authenticator/lib/src/mixins/authenticator_text_field.dart
@@ -21,7 +21,6 @@ mixin AuthenticatorTextField<FieldType extends Enum,
       builder: (BuildContext context, bool toggleObscureText, Widget? _) {
         final obscureText = this.obscureText && toggleObscureText;
         return TextFormField(
-          autovalidateMode: AutovalidateMode.always,
           style: enabled
               ? null
               : TextStyle(color: Theme.of(context).disabledColor),
@@ -38,7 +37,7 @@ mixin AuthenticatorTextField<FieldType extends Enum,
               maxWidth: 70,
             ),
             suffixIcon: suffix,
-            errorMaxLines: errorMaxLines,
+            errorMaxLines: 3,
             hintText: hintText,
           ),
           maxLength: maxLength,
